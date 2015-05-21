58459177404bc176539373cb4cbdf6b2be3e034c
Brian Blais <bblais@gmail.com>
2015-05-21 10:32:49 -0400

    fixed newlines

--- Estimating_Mean_and_Deviation/Estimating_Mean_and_Deviation.py
+++ Estimating_Mean_and_Deviation/Estimating_Mean_and_Deviation.py
@@ -37,15 +37,15 @@
 # and simple flat priors.  We will see how using the correct (Jeffrey's) prior
 # for $\sigma$ will slightly modify our results.  
 # \begin{eqnarray} 
-# p(\mu,\sigma|\bvec{x},I) &\propto& p(\bvec{x}|\mu,\sigma,I) p(\mu,\sigma|I) \\
+# p(\mu,\sigma|\bvec{x},I) &\propto& p(\bvec{x}|\mu,\sigma,I) p(\mu,\sigma|I) \\\\
 # p(\bvec{x}|\mu,\sigma,I)&=&\left(\frac{1}{\sqrt{2\pi\sigma^2}}\right)^N
-# e^{\frac{1}{-2\sigma^2}\sum_{k=1}^{N} (x_k-\mu)^2}\\
+# e^{\frac{1}{-2\sigma^2}\sum_{k=1}^{N} (x_k-\mu)^2}\\\\
 # p(\mu,\sigma|I)&=&{\rm constant} \mbox{ for } \sigma>0 
 # \end{eqnarray}
 # 
 # Plugging into the posterior for the mean we get
 # \begin{eqnarray}
-# p(\mu|\bvec{x},I)&=&\int_0^{\infty} p(\mu,\sigma|\bvec{x},I) d\sigma \\
+# p(\mu|\bvec{x},I)&=&\int_0^{\infty} p(\mu,\sigma|\bvec{x},I) d\sigma \\\\
 # &\propto&\int_0^{\infty} d\sigma
 # \frac{1}{\sigma^N}e^{\frac{1}{-2\sigma^2}\sum_{k=1}^{N} (x_k-\mu)^2}
 # \end{eqnarray}
@@ -53,7 +53,7 @@
 # $d\sigma=-dt/t^2$, and we have
 # \begin{eqnarray}
 # p(\mu|\bvec{x},I)&\propto&\int_0^{\infty} 
-# t^{N-2}e^{\frac{t^2}{2}\sum_{k=1}^{N} (x_k-\mu)^2} dt\\
+# t^{N-2}e^{\frac{t^2}{2}\sum_{k=1}^{N} (x_k-\mu)^2} dt\\\\
 # &\propto& \left[\sum_{k=1}^{N} (x_k-\mu)^2 \right]^{(N-1)/2}
 # \end{eqnarray}
 # 
@@ -116,7 +116,7 @@
 # 
 # If we rewrite the sum $\sum_{k=1}^{N} (x_k-\mu)^2$ as 
 # \begin{eqnarray}
-# \sum_{k=1}^{N} (x_k-\mu)^2 &\equiv&N(\bar{x}-\mu)^2+V \\
+# \sum_{k=1}^{N} (x_k-\mu)^2 &\equiv&N(\bar{x}-\mu)^2+V \\\\
 # V&\equiv& \sum_{k=1}^{N} (x_k-\bar{x})^2
 # \end{eqnarray}
 # 
@@ -130,7 +130,7 @@
 # used the suggested (Jeffreys) prior for the scale parameter, $\sigma$, 
 # 
 # \begin{eqnarray}
-# p(\mu,\sigma|I)&=&\left\{\begin{array}{cc}\frac{1}{\sigma} & \sigma>0 \\
+# p(\mu,\sigma|I)&=&\left\{\begin{array}{cc}\frac{1}{\sigma} & \sigma>0 \\\\
 # 0 & \mbox{otherwise}\end{array}\right.
 # \end{eqnarray}
 # 
@@ -155,20 +155,20 @@
 # 
 # With the Jeffrey's prior,
 # \begin{eqnarray}
-# p(\sigma|\bvec{x},I)&=&\int_{-\infty}^{\infty} p(\mu,\sigma|\bvec{x},I) d\mu \\
+# p(\sigma|\bvec{x},I)&=&\int_{-\infty}^{\infty} p(\mu,\sigma|\bvec{x},I) d\mu \\\\
 # &\propto& \frac{1}{\sigma^{N+1}} e^{-V/2\sigma^2}\underbrace{\int_{-\infty}^{\infty}
-# e^{-\frac{N(\bar{x}-\mu)^2}{2\sigma^2}}d\mu}_{\propto \sigma} \\
+# e^{-\frac{N(\bar{x}-\mu)^2}{2\sigma^2}}d\mu}_{\propto \sigma} \\\\
 # &\propto& \frac{1}{\sigma^{N}} e^{-V/2\sigma^2}
 # \end{eqnarray}
 # 
 # If we change variables to $\xi\equiv V/\sigma^2$, then this is the $\chi^2$
 # distribution with $f\equiv N-1$ degrees of freedom:
 # \begin{eqnarray}
-# d\sigma &\propto& \xi^{-3/2} \\
+# d\sigma &\propto& \xi^{-3/2} \\\\
 # p(\xi|\bvec{x},I)&=&p(\sigma|\bvec{x},I)\times
-# \left|\frac{d\sigma}{d\xi}\right| \\
-# &\propto& \xi^{\frac{N}{2}} e^{-\xi/2}\times \xi^{-3/2} \\
-# &\propto& \xi^{\frac{N-1}{2}-1} e^{-\xi/2} \\
+# \left|\frac{d\sigma}{d\xi}\right| \\\\
+# &\propto& \xi^{\frac{N}{2}} e^{-\xi/2}\times \xi^{-3/2} \\\\
+# &\propto& \xi^{\frac{N-1}{2}-1} e^{-\xi/2} \\\\
 # &\propto& \xi^{\frac{f}{2}-1} e^{-\xi/2} 
 # \end{eqnarray}
 # 
@@ -176,21 +176,21 @@
 # 
 # With the uniform prior,
 # \begin{eqnarray}
-# p(\sigma|\bvec{x},I)&=&\int_{-\infty}^{\infty} p(\mu,\sigma|\bvec{x},I) d\mu \\
+# p(\sigma|\bvec{x},I)&=&\int_{-\infty}^{\infty} p(\mu,\sigma|\bvec{x},I) d\mu \\\\
 # &\propto& \frac{1}{\sigma^{N}} e^{-V/2\sigma^2}\underbrace{\int_{-\infty}^{\infty}
-# e^{-\frac{N(\bar{x}-\mu)^2}{2\sigma^2}}d\mu}_{\propto \sigma} \\
+# e^{-\frac{N(\bar{x}-\mu)^2}{2\sigma^2}}d\mu}_{\propto \sigma} \\\\
 # &\propto& \frac{1}{\sigma^{N-1}} e^{-V/2\sigma^2}
 # \end{eqnarray}
 # 
 # \begin{eqnarray}
-# L(\sigma) &=& {\rm constant}-(N-1)\log \sigma - \frac{V}{2\sigma^2} \\
-# \frac{dL}{d\sigma} &=& -\frac{N-1}{\sigma}+\frac{V}{\sigma^3} = 0 \\
-# \Rightarrow \hat{\sigma^2} &=& \frac{V}{N-1} \\
+# L(\sigma) &=& {\rm constant}-(N-1)\log \sigma - \frac{V}{2\sigma^2} \\\\
+# \frac{dL}{d\sigma} &=& -\frac{N-1}{\sigma}+\frac{V}{\sigma^3} = 0 \\\\
+# \Rightarrow \hat{\sigma^2} &=& \frac{V}{N-1} \\\\
 # \left.\frac{d^2L}{d\sigma^2}\right|_{\hat{\sigma}} &=&
-# \frac{N-1}{\hat{\sigma}^2}-\frac{3V}{\hat{\sigma}^4} \\
-# &=&\frac{N-1}{\hat{\sigma}^2}-\frac{3(N-1)}{\hat{\sigma}^2} \\
-# &=&-\frac{2(N-1)}{\hat{\sigma}^2} \\
-# {\rm width}&=&-\left[\left.\frac{d^2L}{d\sigma^2}\right|_{\hat{\sigma}}\right]^{-1/2} \\
+# \frac{N-1}{\hat{\sigma}^2}-\frac{3V}{\hat{\sigma}^4} \\\\
+# &=&\frac{N-1}{\hat{\sigma}^2}-\frac{3(N-1)}{\hat{\sigma}^2} \\\\
+# &=&-\frac{2(N-1)}{\hat{\sigma}^2} \\\\
+# {\rm width}&=&-\left[\left.\frac{d^2L}{d\sigma^2}\right|_{\hat{\sigma}}\right]^{-1/2} \\\\
 # &=& \frac{\hat{\sigma}}{\sqrt{2(N-1)}}
 # \end{eqnarray}
 # 
@@ -200,9 +200,9 @@
 # So our final estimate for the mean and standard deviation is
 # 
 # \begin{eqnarray}
-# \mu&=&\bar{x} \pm \frac{S}{\sqrt{N}} \\
-# \bar{x}&=& \frac{1}{N} \sum_{k=1}^{N} x_k \\
-# S^2&=& \frac{1}{(N-1)}\sum_{k=1}^{N} (x_k-\bar{x})^2 \\
+# \mu&=&\bar{x} \pm \frac{S}{\sqrt{N}} \\\\
+# \bar{x}&=& \frac{1}{N} \sum_{k=1}^{N} x_k \\\\
+# S^2&=& \frac{1}{(N-1)}\sum_{k=1}^{N} (x_k-\bar{x})^2 \\\\
 # \sigma&=& S^2 \pm \frac{S^2}{\sqrt{2(N-1)}}
 # \end{eqnarray}
 # 
